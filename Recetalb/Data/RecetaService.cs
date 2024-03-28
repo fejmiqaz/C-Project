4e4940990a664c486d0709e5ec2670d20ee824a5
Fejmi <fejmiqaz@gmail.com>
2024-03-28 02:07:01 +0100

    Add files via upload

---
+++ Recetalb/Data/RecetaService.cs
@@ -0,0 +1,23 @@
+﻿namespace Recetalb.Data
+
+{
+    public class RecetaService
+    {
+        private readonly HttpClient _httpClient;
+        private string? _apiBaseUrl;
+        private readonly string _recetaService = "";
+
+        public RecetaService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            _apiBaseUrl = configuration["ApiBaseUrl"];
+        }
+
+
+        public async Task<T> GetAsync<T>(string endpoint, T response)
+        {
+            var respone = await _httpClient.GetFromJsonAsync<T>(_recetaService + endpoint);
+            return response;
+        }
+    }
+}
