c66b8f3ffda82d2a599d9a0d13a4f0aaa44cf0f4
Fejmi <fejmiqaz@gmail.com>
2024-03-28 02:16:11 +0100

    Delete Recetabl API directory

--- Recetabl API/DTO/EditKategoriaDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ASP.NET_Core_6._0_API.DTO
-{
-    public class EditKategoriaDTO:CreateKategoriaDTO
-    {
-        public int Id { get; set; }
-    }
-}
--- Recetabl API/DTO/EditPerberesiDTO.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ASP.NET_Core_6._0_API.DTO
-{
-    public class EditPerberesiDTO:CreatePerberesiDTO
-    {
-        public int Id { get; set; }
-    }
-}
